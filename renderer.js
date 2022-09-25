1a0212ab1e5117c781c56d0b12e532b4a25f3208
kinakomoti-quad <52781608+kinakomoti-quad@users.noreply.github.com>
2022-09-25 14:51:29 +0900

    set fullscreen off

--- renderer.js
+++ renderer.js
@@ -7,7 +7,7 @@
  */
 
 var lever = false; //レバー入力
-const debug_mode = false //デバッグモード、いずれグローバルから取得したい
+const debug_mode = true //デバッグモード、いずれグローバルから取得したい
 
  //シリアル通信
 const { SerialPort } = require('serialport')
